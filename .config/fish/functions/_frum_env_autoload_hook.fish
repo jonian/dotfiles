94667197b8569b709649e67c13e68768cbe76159
jonian <jonian@hardpixel.eu>
2021-08-27 02:29:18 +0300

    update fish functions

--- .config/fish/functions/_frum_env_autoload_hook.fish
+++ .config/fish/functions/_frum_env_autoload_hook.fish
@@ -1,3 +1,3 @@
 function _frum_env_autoload_hook --on-variable PWD --description 'Change Ruby env variables on directory change'
-  set -Ux GEM_HOME (ruby -e 'puts Gem.user_dir')
+  set -gx GEM_HOME (ruby -e 'puts Gem.user_dir')
 end
