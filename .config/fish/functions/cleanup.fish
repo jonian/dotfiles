c925b7290693d369fa690bdc333e280eff0d1ed1
jonian <jonian@hardpixel.eu>
2021-09-10 02:38:34 +0300

    update fish functions

--- .config/fish/functions/cleanup.fish
+++ .config/fish/functions/cleanup.fish
@@ -3,8 +3,15 @@ function cleanup
   command profile-cleaner b
   command profile-cleaner gc
 
+  command rm -rf ~/.bundle/cache
+  command rm -rf ~/.frum/versions/.downloads
+
+  command yarn cache clean
+  command npm cache clean --force
+
+  command flatpak uninstall --unused
+  command yay -Scc --noconfirm
+
   command sudo journalctl --rotate
   command sudo journalctl --vacuum-time 1s
-
-  command yay -Scc --noconfirm
 end
